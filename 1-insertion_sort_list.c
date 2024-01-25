990cefcb05a1b994daa64ec70a05f7f8e3a74b13
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-01-25 20:46:06 +0100

    insert sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -27,28 +27,31 @@ void swap(listint_t *a, listint_t *b)
  * Return: void
  */
 void insertion_sort_list(listint_t **list)
-{
-	listint_t *i, *j;
-	int n;
 
-	if (!list || !*list || !(*list)->next)
-		return;
-	i = (*list)->next;
-	while (i)
+{
+	listint_t *t = *list, *swap, *j;
+
+	j = *list;
+	while (j)
 	{
-		j = i;
-		i = i->next;
-		while (j && j->prev)
+		t = j;
+		while (t->prev && t->prev->n > t->n)
 		{
-			if (j->prev->n > j->n)
-			{
-				swap(j->prev, j);
-				if (!j->prev)
-					*list = j;
-				print_list(const listint_t *list);
-			}
-			else
-				j = j->prev;
+			swap = t->prev;
+
+			t->prev = swap->prev;
+			swap->next = t->next;
+			if (t->prev)
+				t->prev->next = t;
+			swap->prev = t;
+			t->next = swap;
+			if (swap->next)
+				swap->next->prev = swap;
+			if (!t->prev)
+				*list = t;
+			t = swap->prev;
+			print_list(*list);
 		}
+		j = j->next;
 	}
 }
