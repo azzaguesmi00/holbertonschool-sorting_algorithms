a4b26d15d8ec78b71bf9f3c2706e6458fbf0abb8
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-01-25 20:57:41 +0100

    selectsort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,26 +7,26 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, k;
-    int n;
+	size_t i, j, k;
+	int n;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        k = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[k])
-            {
-                k = j;
-                continue;
-            }
-        }
-        if (k != i)
-        {
-            n = array[i];
-            array[i] = array[k];
-            array[k] = n;
-            print_array(array, size);
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		k = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[k])
+			{
+				k = j;
+				continue;
+			}
+		}
+		if (k != i)
+		{
+			n = array[i];
+			array[i] = array[k];
+			array[k] = n;
+			print_array(array, size);
+		}
+	}
 }
